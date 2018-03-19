66e5967e3574981b795711824c0dd36745537706
Ayesha Rao <ayesha14.1.97@gmail.com>
2018-03-19 17:44:00 +0530

    `

--- src/utilities/uportSetup.js
+++ src/utilities/uportSetup.js
@@ -1,8 +1,8 @@
 import { Connect, SimpleSigner } from 'uport-connect'
 
-const uport = new Connect('SSI Demo App',{
-  clientId: '2oeXufHGDpU51bfKBsZDdu7Je9weJ3r7sVG',
-  signer: SimpleSigner('c818c2665a8023102e430ef3b442f1915ed8dc3abcaffbc51c5394f03fc609e2')
+const uport = new Connect('AIT',{
+  clientId: '2ozo1SznXHTQTvf1RhgLGegpx6w5LVXsRQ9',
+  signer: SimpleSigner('734dfdf5582fd3583a3d38857e05a66913ef46cc5dedb2dc84a404e6b8817148')
 })
 
 const web3 = uport.getWeb3()
